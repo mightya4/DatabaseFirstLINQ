7c332fae400b538ec74ea229975a5becac38c314
devin <devinsymone@gmail.com>
2022-05-11 15:35:18 -0400

    problem 13

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -27,7 +27,7 @@ namespace DatabaseFirstLINQ
             //            //ProblemTen();
                             ProblemEleven();
                             ProblemTwelve();
-            //            //ProblemThirteen();
+                            ProblemThirteen();
             //            //ProblemFourteen();
             //            //ProblemFifteen();
             //            //ProblemSixteen();
@@ -200,19 +200,19 @@ namespace DatabaseFirstLINQ
 
         }
 
-        //        private void ProblemThirteen()
-        //        {
-        //            // Add the role of "Customer" to the user we just created in the UserRoles junction table using LINQ.
-        //            var roleId = _context.Roles.Where(r => r.RoleName == "Customer").Select(r => r.Id).SingleOrDefault();
-        //            var userId = _context.Users.Where(u => u.Email == "david@gmail.com").Select(u => u.Id).SingleOrDefault();
-        //            UserRole newUserRole = new UserRole()
-        //            {
-        //                UserId = userId,
-        //                RoleId = roleId
-        //            };
-        //            _context.UserRoles.Add(newUserRole);
-        //            _context.SaveChanges();
-        //        }
+                private void ProblemThirteen()
+                {
+                    // Add the role of "Customer" to the user we just created in the UserRoles junction table using LINQ.
+                    var roleId = _context.Roles.Where(r => r.RoleName == "Customer").Select(r => r.Id).SingleOrDefault();
+                    var userId = _context.Users.Where(u => u.Email == "david@gmail.com").Select(u => u.Id).SingleOrDefault();
+                    UserRole newUserRole = new UserRole()
+                    {
+                        UserId = userId,
+                        RoleId = roleId
+                    };
+                    _context.UserRoles.Add(newUserRole);
+                    _context.SaveChanges();
+                }
 
         //        private void ProblemFourteen()
         //        {
